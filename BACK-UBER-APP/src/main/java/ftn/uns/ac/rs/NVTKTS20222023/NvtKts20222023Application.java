24977c833463ad87e170746486f97be0493c2491
kaprin <vaskebonf22@gmail.com>
2023-02-11 10:40:29 +0100

    Project initialization.

---
+++ BACK-UBER-APP/src/main/java/ftn/uns/ac/rs/NVTKTS20222023/NvtKts20222023Application.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.NVTKTS20222023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvtKts20222023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvtKts20222023Application.class, args);
+	}
+
+}
---
+++ BACK-UBER-APP/src/test/java/ftn/uns/ac/rs/NVTKTS20222023/NvtKts20222023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.NVTKTS20222023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvtKts20222023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
